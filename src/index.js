23aa1f29bfdfddc65b2cb59e36afd09488348262
Raveena Ingale <ingale.r@northeastern.edu>
2025-01-24 19:44:05 -0500

    meal completed

--- src/index.js
+++ src/index.js
@@ -18,8 +18,6 @@ try {
             }
         }
         pantry.ingredients.push(obj);
-        console.log(pantry.ingredients);
-
         localStorage.setItem("pantry", JSON.stringify(pantry));
         formElem.reset();
     })
@@ -138,11 +136,67 @@ try {
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("weight-tablebody") && loadWeightTable();
     document.getElementById("weight-tablebody") && lastSevenAverage()
-
 });
 
+const removeRow = (id) => {
+    const [name, calories,] = id.split("-");
+    const getRow = document.getElementById(id);
+    let previousSize = getRow.getAttribute('value');
+
+    getRow.remove();
+    console.log(previousSize);
+    renderTotalCalories(-calories * previousSize);
+}
+
+const updateServingSize = (event, id, calories) => {
+
+    const inputServingSize = document.getElementById(id);
+    let newSize = event.target.value || 0;
+
+    // Compute Total calories and render it
+    let previousServingSize = inputServingSize.getAttribute('value')
+    let previousCalories = calories * previousServingSize;
+    let newCalories = calories * newSize;
+
+    renderTotalCalories(- previousCalories + newCalories);
+
+
+    // Update the new Previous value
+    inputServingSize.setAttribute('value', newSize);
+}
+
+const renderSelectedItemAsRow = (name, calories, size, unit) => {
+    return `<tr id="${name}-${calories}-${size}" value="${size}">
+                <td>${name}</td>
+                <td>${calories} kcal</td>
+                <td>
+                    <input oninput="updateServingSize(event, '${name}-${calories}-${size}', '${calories}')" style="width: 40%;" type="number" name="serving-size-${name}" value="${size}"></input>
+                </td>
+                <td>${unit}</td>
+                <td onclick="removeRow('${name}-${calories}-${size}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                </svg></td>
+            </tr>`
+}
+
+
+let totalCalories = 0;
+
+const renderTotalCalories = (calories) => {
+    totalCalories += calories;
+    const totalCalorieSpan = document.getElementById("total-calories");
+    totalCalorieSpan.innerText = `Total Calories : ${totalCalories} Kcal`;
+}
+
+const addSelectedIngredientToTable = (name, calories, size, unit) => {
+    const tableSelectedIngredient = document.getElementById("selected-ingredient-table");
+    tableSelectedIngredient.innerHTML += renderSelectedItemAsRow(name, calories, size, unit);
+    renderTotalCalories(calories * size);
+}
+
 const displayListIngredient = (ingredient) => {
-    return ` <li class="list-group-item d-flex justify-content-between align-items-start">
+    return ` <li onclick="addSelectedIngredientToTable('${ingredient.name}','${ingredient.calories}', '${ingredient.servingSize}', '${ingredient.servingUnit}' )" class="list-group-item d-flex justify-content-between align-items-start">
             <div class="ms-2 me-auto">
             <div class="fw-bold">${ingredient.name}</div>
             ${ingredient.description}
@@ -174,7 +228,45 @@ try {
         displayFilteredIngredients(filteredIngredients);
     })
 } catch (error) {
+}
+
+try {
+
+    const mealFormElem = document.getElementById("mealForm");
+    mealFormElem.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(mealFormElem);
+        let meal = {
+            mealName: formData.get("mealname"),
+            description: formData.get("description"),
+            servingSize: formData.get("servingsize"),
+            ingredients: [],
+        }
+        //processing dynamic ingredients:
+        for (const pair of formData.entries()) {
+            const key = pair[0];
+            const servingSize = pair[1];
+            if (key.includes("serving-size-")) {
+                const name = key.split("-")[2];
+                let ingredient = {
+                    name,
+                    servingSize
+                };
+                meal.ingredients.push(ingredient);
+            }
+        }
+
+        pantry.meals.push(meal);
+        localStorage.setItem("pantry", JSON.stringify(pantry));
+        mealFormElem.reset();
+        alert('Meal added succesfully!');
+
+    })
+
+} catch (error) {
 
 }
+
+
 
 
